352e461b0b0062e7f83e02f31672972a924af556
syed sufiyan ali <sufiyantdc@gmail.com>
2026-09-21 21:06:24 +0530

    list in python

---
+++ List and Dictionaries/list.py
@@ -0,0 +1,30 @@
+marks= [92,23,54,78,54,99]
+print(marks)
+print(type(marks))
+print(marks[2])
+
+student = ["ali",287,829.98,"hyderabad"]
+print(student)
+student[0]="sufiyan"
+print(student)
+
+list = [1,2,3,4]
+list.append(5)
+print(list)
+list.append(0)
+list.sort()
+print(list)
+list.sort(reverse=True)
+print(list)
+
+fruits = ["apple","mango","grapes"]
+fruits.append("banana")
+fruits.reverse()
+print(fruits)
+
+
+numbers= [1,24,3,24,5]
+numbers.remove(24)
+print(numbers)
+numbers.pop(0)
+print(numbers)
